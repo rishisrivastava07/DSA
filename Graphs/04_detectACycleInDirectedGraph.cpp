a8793ffee0ebe856eaf88495ebdab803fc2afd81
Rishi Srivastava <rishisrivastava.ju@gmail.com>
2024-07-04 08:35:05 +0530

    Detect cycle in directed graph using BFS

--- Graphs/04_detectACycleInDirectedGraph.cpp
+++ Graphs/04_detectACycleInDirectedGraph.cpp
@@ -16,15 +16,55 @@ class Solution {
         inRecursion[u] = false;
         return false;
     }
+    bool bfsCycle(int V, vector<int> adj[]){
+        int count = 0;
+	    vector<int> inDegree(V, 0);
+	    
+	    for(int i = 0; i < V; i++){
+	        for(auto it : adj[i]){
+	            inDegree[it]++;
+	        }
+	    }
+	    
+	    queue<int> q;
+	    for(int i = 0; i < V; i++){
+	        if(inDegree[i] == 0){
+	            q.push(i);
+	            count++;
+	        }
+	    }
+	    
+	    while(!q.empty()){
+	        int currNode = q.front();
+	        q.pop();
+	        
+	        for(auto adjNodes : adj[currNode]){
+	            inDegree[adjNodes]--;
+	            if(inDegree[adjNodes] == 0){
+	                q.push(adjNodes);
+	                count++;
+	            }
+	        }
+	    }
+	    
+	    if(count == V) return false;
+	    return true;
+    }
   public:
     // Function to detect cycle in a directed graph.
     bool isCyclic(int V, vector<int> adj[]) {
         // code here
         vector<bool> vis(V, 0);
         vector<bool> inRecursion(V, 0);
+
+        // using DFS
         for(int i = 0; i < V; i++){
             if(!vis[i] && dfsCycle(i, adj, vis, inRecursion)) return true;
         }
+
+        // using BFS
+        if(bfsCycle(V, adj)) return true;
+        
         return false;
     }
 };
