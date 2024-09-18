4ef9a7c1903004ebbedd343fe0c94f9e97ac1c4a
Rishi Srivastava <rishisrivastava.ju@gmail.com>
2024-09-18 15:24:39 +0530

    String Day 01 - Integer to Roman Conversion

--- Strings/04_integerToRoman.cpp
+++ Strings/04_integerToRoman.cpp
@@ -5,22 +5,15 @@ class Solution {
 public:
     string intToRoman(int num) {
         vector<pair<int, string>> i2R = {{1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"}, {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"}, {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}};
-
         string result = "";
+        int i = 0;
         while(num > 0){
-            for(int i = 0; i < 13; i++){
-                if(num >= i2R[i].first){
-                    int times = num / i2R[i].first;
-                    num %= i2R[i].first;
-
-                    while(times-- > 0){
-                        result += i2R[i].second;
-                    }
-                    break;
-                }
+            while(num >= i2R[i].first){
+                num -= i2R[i].first;
+                result += i2R[i].second;
             }
+            i++;
         }
-
         return result;
     }
 };
